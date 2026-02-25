cf4fcb8dc5aa04f8d126868f4d4814cc3eaa9bb1
SathanT <sathantthangavel@gmail.com>
2026-02-25 17:05:00 +0530

    Remove sensitive database URL from database.py
    Removed database URL for security reasons.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-db_url = "mysql+pymysql://root:root%40123@localhost:3306/task_db"
+db_url = ""
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
