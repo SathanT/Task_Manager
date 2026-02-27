17bdbb3389caf30de0e4a67e3790afcba14e6d7e
SathanT <sathantthangavel@gmail.com>
2026-02-27 09:28:41 +0530

    removed duplicate files

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-db_url = ""
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-Base = declarative_base()
